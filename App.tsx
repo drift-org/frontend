f97fe164b8b7191c9cc8030d28aa81b8ee39163b
PrateekS1917 <61072843+PrateekSane@users.noreply.github.com>
2021-05-27 17:39:56 -0700

    ultra scuffed signup page
    not PR ready

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Signup from './components/auth/signup.component'
+import TempHome from './components/general/temphome.component'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( 
+    <Router> 
+      <View style={styles.appcontainer}>
+        <Route exact path='/' component={TempHome}/>
+        <Route path='/signup' component={Signup}/>
+      </View>
+    </Router>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appcontainer: {
+    height: '100%',
+    width: '100%'
   },
 });
