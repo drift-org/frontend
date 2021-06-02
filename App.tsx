d87b788e56ab49ae7328bed5e1a8476a4932ed0e
= <anjanbharadwaj02@gmail.com>
2021-06-01 22:17:21 -0700

    Removing comments

--- App.tsx
+++ App.tsx
@@ -1,17 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Homepage from "./components/homepage/homepage.component";
 
 export default function App() {
-  return (
-    <Homepage />
-    // <View style={styles.container}>
-    //   <Text>Open up App.tsx to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    //   <Homepage />
-    // </View>
-  );
+  return <Homepage />;
 }
 
 const styles = StyleSheet.create({
