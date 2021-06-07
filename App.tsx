f07c369737b972ba7f9fd1dbf101f9c85975e558
Brandon Li <brandon.li820@gmail.com>
2021-06-07 08:26:37 -0700

    Completed wireframe for Find Friends page and added pages functionality (#14)
    * completed wireframe for find friends
    
    CoAuthored by Brandon Li <brandon.li820@gmail.com>
    
    * implemented pages for react native
    
    * add homepage dir
    
    Co-authored-by: Kenzie Tee <teekenzie@berkeley.edu>

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Homepage from "./components/homepage/homepage.component";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FindFriends from './components/homepage/find-friends.component';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <Homepage />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='FindFriends'
+          component={FindFriends}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
