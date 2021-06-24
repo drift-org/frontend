2a7aa4c7aafea174ace9b80f4cec1502fb7fd95d
Kenzie Tee <teekenzie@berkeley.edu>
2021-06-23 22:42:18 -0700

    added NunitoSans to font family
    Co-authored-by: Brandon Li <brandon.li@berkeley.edu>
    Co-authored-by: Kenzie Tee<teekenzie@berkeley.edu>

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import FindFriends from './pages/find-friends.component';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { registerRootComponent } from 'expo';
+import { useFonts } from '@expo-google-fonts/nunito-sans';
+import * as NunitoSans from '@expo-google-fonts/nunito-sans';
 const Stack = createStackNavigator();
 
 function App() {
+  useFonts(NunitoSans);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
